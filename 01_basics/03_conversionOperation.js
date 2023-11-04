d7f2d60b5def5f9bac3bb6cc0601f418f9260424
sudhakar pal <98749950+sudhakar010@users.noreply.github.com>
2023-11-04 18:05:01 +0000

    for the conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+//  "33abc" => NaN
+// true => 1
+//  false => 0
+
+let isloggedin = 1;
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+// 1 => true ; 0 => false
+// "" => false; 
+// "hitesh" => true; 
+
+let number = 34
+let stringnumber = String(number)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+// ok here conversion for the string.
