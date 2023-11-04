c248e7f46dab005cb41757cfbcd338a4a3b8b470
sudhakar pal <98749950+sudhakar010@users.noreply.github.com>
2023-11-04 16:00:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all JS code as newer version
+
+// alert (3+3) // we are using node js , not browser
+
+// we have keep the readiablity of code in mind.
+
+let name = "sudhakar"
+let age = 21
+let isloogedIn = false
+
+// datatypes
+// number => 2 to power 53
+// bigint
+// string  => ""
+// boolean => true/false
+// null => it is standalone value
+// undefined =>  abhi value assigned nhi hui hai
+// symbol => for unique
+
+
+// object
+
+// null ka type is object 
+// baaki sab ka same 
+// like undefined ka type is undefined only: 
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+console.log(typeof "sudh");
+
+
