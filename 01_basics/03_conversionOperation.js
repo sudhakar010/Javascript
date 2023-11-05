42ed85d84846631925c10912b3b1b3674b68bbf3
sudhakar pal <98749950+sudhakar010@users.noreply.github.com>
2023-11-05 05:17:08 +0000

    about conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,43 @@ let stringnumber = String(number)
 console.log(stringnumber);
 console.log(typeof stringnumber);
 // ok here conversion for the string.
+
+ //****** Operations ********//
+
+
+ let value = 3;
+ let negValue = -value
+ console.log(negValue)
+
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**2);
+ console.log(2/2);
+
+ let srt1 = "hello"
+ let str2 = "hitesh"
+
+ let str3 = str1 + str2
+ console.log(str3);
+
+
+
+//  console.log("1" + 2);
+//  console.log(1 + "2");
+//  console.log("1" + 2 + 2);// phle string hoga to sabko string ki tarah treat kiya jayga
+//  console.log(1 + 2 + "2"); // agar last mein string hoga to phle sabko normally treat kiya jayega
+
+//  console.log((3+4)*5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2 // not a good method to declare a variable
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
+
